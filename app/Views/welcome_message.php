aa6b2e013cb280aa67b9e7cccc4bfc2840e4f845
=sub021 <=>
2023-08-07 12:50:38 +0800

    commit 07/08/23

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -7,7 +7,8 @@
     <meta name="description" content="The small framework with powerful features">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" type="image/png" href="/favicon.ico">
-
+    <?php
+    header('Location: ' . route_to('dashboard')) ?>
     <!-- STYLES -->
     <?php
     // dd(auth()->loggedIn());
